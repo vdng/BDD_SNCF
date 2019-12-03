b5ad709e05c2c256238ecd70811a121949d677b0
vdng <duong.vincent@outlook.fr>
2019-12-03 23:42:56 +0100

    database

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'HEROKU_POSTGRESQL_MAUVE_URL') or 'mysql+pymysql://root:mdp@127.0.0.1:3306/sncf'
+        'DATABASE_URL') or 'mysql+pymysql://root:mdp@127.0.0.1:3306/sncf'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
